681ecc1fafef6825032177466a8dd620482e658e
liqiang <qiangli_i697@anjuke.com>
2016-05-25 17:45:51 +0800

    将方法参数提取到properties文件中

--- src/main/java/edu/shu/nlp/skytorif/utils/ImageProcessing.java
+++ src/main/java/edu/shu/nlp/skytorif/utils/ImageProcessing.java
@@ -4,43 +4,65 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 import javax.imageio.ImageIO;
 
-public class ImageProcessing {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	/**
-	 * 图片去水印（logo位置固定，图片处理后的默认格式.jpg）
-	 * @param srcImagePath 待处理图片的路径
-	 * @param toImagePath 处理后图片的保存路径
-	 * @param pixelNumbers 待更新的像素数
-	 * @param x 图片中水印所在x轴位置（左上角的x坐标）
-	 * @param y 图片中水印所在y轴位置（左上角的y坐标）
-	 * @param logo_width 图片中水印的水平长度
-	 * @param logo_height 图片中水印的垂直高度
-	 * @throws IOException
-	 */
-	public static void removeLogoFromImage(String srcImagePath, String toImagePath, int pixelNumbers, int x, int y, int logo_width, int logo_height) throws IOException{
+public class ImageProcessing {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ImageProcessing.class);
+	//图片纵向的像素分割数量，越大图片表现出的纵向区别越大，应小于图片的高度
+	private static int pixelNumbers;
+	private static String srcImagePath;
+	private static String toImagePath;
+	private static int logo_point_x;
+	private static int logo_point_y;
+	private static int logo_width;
+	private static int logo_height;
+	
+	static{
+		Properties prop = new Properties();
+		InputStream in = ImageProcessing.class.getResourceAsStream("para.properties");
+		try {
+			prop.load(in);
+		} catch (IOException e) {
+			logger.error("加载配置文件出错！");
+			e.printStackTrace();
+		}
+		pixelNumbers = Integer.parseInt(prop.getProperty("pixelNumbers"));
+		srcImagePath = prop.getProperty("srcImagePath");
+		toImagePath = prop.getProperty("toImagePath");
+		logo_point_x = Integer.parseInt(prop.getProperty("logo_point_x"));
+		logo_point_y = Integer.parseInt(prop.getProperty("logo_point_y"));
+		logo_width = Integer.parseInt(prop.getProperty("logo_width"));
+		logo_height = Integer.parseInt(prop.getProperty("logo_height"));
+		if(logger.isDebugEnabled()){
+			logger.debug("pixelNumbers: " + pixelNumbers);
+			logger.debug("srcImagePath: " + srcImagePath);
+			logger.debug("toImagePath: " + toImagePath);
+			logger.debug("logo_point_x: " + logo_point_x);
+			logger.debug("logo_point_y: " + logo_point_y);
+			logger.debug("logo_width: " + logo_width);
+			logger.debug("logo_height: " + logo_height);
+		}
+	}
+	
+	public static void removeLogoFromImage() throws IOException{
 		
 		File imageFile = new File(srcImagePath);
 		BufferedImage image = ImageIO.read(imageFile);
 		
-		verticalAlgorithm(image, pixelNumbers, x, y, logo_width, logo_height);
+		verticalAlgorithm(image);
 		
 		FileOutputStream output = new FileOutputStream(toImagePath);		
 		ImageIO.write(image, "jpg", output);
 	} 
 	
-	/**
-	 * 图片去水印的算法：垂直处理
-	 * @param image 待处理的图片
-	 * @param pixelNumbers 待更新的像素数
-	 * @param x 图片中水印所在x轴位置（左上角的x坐标）
-	 * @param y 图片中水印所在y轴位置（左上角的y坐标）
-	 * @param logo_width 图片中水印的水平长度
-	 * @param logo_height 图片中水印的垂直高度
-	 */
-	private static void verticalAlgorithm(BufferedImage image, int pixelNumbers, int x, int y, int logo_width, int logo_height){
+	private static void verticalAlgorithm(BufferedImage image){
 		
 		int height = logo_height; 
 		int point_y = 1; //需要重复的像素点y轴坐标的偏移量
@@ -55,14 +77,18 @@ public class ImageProcessing {
 		}while(point_y <= height && cur < s);
 		//core
 		for(int i = 0; i < s; i++){
-			for(int j = x; j < x + logo_width; j++){
+			for(int j = logo_point_x; j < logo_point_x + logo_width; j++){
 				for(int k = 0; k < pixelNumbers; k++){
-					if(y + sections[i] + k <= y + logo_height){
-						image.setRGB(j, y + sections[i] + k, image.getRGB(j, y + sections[i]));
+					if(logo_point_y + sections[i] + k <= logo_point_y + logo_height){
+						image.setRGB(j, logo_point_y + sections[i] + k, image.getRGB(j, logo_point_y + sections[i]));
 					}
 				}
 			}
 		}
 		
+	}
+	
+	public static void main(String[] args) throws IOException{
+		ImageProcessing.removeLogoFromImage();
 	}
 }
